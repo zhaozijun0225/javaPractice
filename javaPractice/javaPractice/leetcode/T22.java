ce9459179dd1b007f35b56af61006e57e58a9eb8
zzj <981112782@qq.com>
2025-08-27 18:10:05 +0800

    2025.08.27 T22

---
+++ javaPractice/javaPractice/leetcode/T22.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import java.util.*;
+
+public class T22 {
+    public static void main(String[] args) {
+        int n = 3;
+        List<String> ans = new ArrayList<>();
+        
+    }
+}
