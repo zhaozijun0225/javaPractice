f18a521be81f278fb7582f22d392ded5c07f4e24
zzj <981112782@qq.com>
2025-11-19 16:46:46 +0800

    20251118 T230 T239

--- javaPractice/javaPractice/leetcode/T230.java
+++ javaPractice/javaPractice/leetcode/T230.java
@@ -1,6 +1,9 @@
 package leetcode;
 
-public class T239 {
+import java.util.*;
+
+
+public class T230 {
     static public class TreeNode {
         int val;
         TreeNode left;
@@ -20,24 +23,28 @@ public class T239 {
         }
     }
 
-    static int ans = Integer.MIN_VALUE;
-
-    static int search(TreeNode root) {
-        if (root == null)
-            return 0;
-        int l = Math.max(search(root.left),0), r = Math.max(search(root.right),0);
-        ans = Math.max(ans, l + root.val + r);
-
-
-        return Math.max(l + root.val, r + root.val);
-    }
-
-
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
+        int k = 1;
+
+        int ans;
+        Deque<TreeNode> st = new ArrayDeque<TreeNode>();
+        TreeNode tmpTN1 = root;
+        while (!st.isEmpty() || tmpTN1!=null) {
+            while (tmpTN1 != null) {
+                st.push(tmpTN1);
+                tmpTN1 = tmpTN1.left;
+            }
+            tmpTN1 = st.pop();
+            k--;
+            if(k==0){
+                ans = tmpTN1.val;
+            }
+            tmpTN1 = tmpTN1.right;
 
 
-        search(root);
+
+        }
 
         return ans;
 
